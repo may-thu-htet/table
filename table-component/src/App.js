99a7caec8daeb2a4852bf869ae65eced27c0469a
May Thu Htet <maythuhtet2892@gmail.com>
2024-10-10 14:33:27 +0100

    initializing landing page

--- table-component/src/App.js
+++ table-component/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/amazon-list">Amazon Questions list</Link>
+          </li>
+          <li>
+            <Link to="/google-list">Google Questions list</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/amazon-list" element=""></Route>
+        <Route path="/google-list" element=""></Route>
+      </Routes>
+    </Router>
   );
 }
 
