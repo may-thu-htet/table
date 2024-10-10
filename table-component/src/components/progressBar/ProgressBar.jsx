fc7e0d3e49b7fb2088ed24ce754863f91d83f631
May Thu Htet <maythuhtet2892@gmail.com>
2024-10-10 14:56:13 +0100

    progress bar creation

---
+++ table-component/src/components/progressBar/ProgressBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProgressBar = ({ value }) => {
+  return (
+    <div className="progress-bar-container">
+      <div className="progress-bar"></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
